715cbcbfd093609ec724fdb2866c37d43dc1e2b3
Ben Solum <benjamin@soluml.com>
2017-01-31 21:12:09 -0600

    Error handling is much better

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+/* eslint no-console: 0, global-require: 0, no-cond-assign: 0, import/no-dynamic-require: 0 */
+
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+const codeFrame = require('babel-code-frame');
 const PreStyle = require('pre-style');
 
 module.exports = function BabelPluginPreStyle ({ types: t }) {
@@ -37,26 +41,31 @@ module.exports = function BabelPluginPreStyle ({ types: t }) {
         if (fpath.node.tag.name !== 'PreStyle') return;
         const config = getConfig(state);
         const css = fpath.node.quasi.quasis[0].value.raw;
+        const cf = codeFrame(fpath.hub.file.code, fpath.node.loc.start.line, fpath.node.loc.start.column, { highlightCode: true });
 
-        Promise.reject().then((data) => {
+        PreStyle(css, config).then((data) => {
           console.log('SUCCESS');
           console.log(data);
-        }).catch((e) => {
-          throw fpath.buildCodeFrameError('PreStyle ran into errors processing the following CSS:');
-        }).catch((e) => {
-          process.stdout.write(e);
+        }, (e) => {
+          console.log(chalk.red(`The PreStyle Adapter ran into an error: ${chalk.bold(e)}`));
+          console.log(cf);
+          process.exit();
         });
       },
       JSXElement(fpath, state) {
         if (fpath.node.openingElement.name.name !== 'PreStyle' || fpath.node.closingElement.name.name !== 'PreStyle') return;
         const config = getConfig(state);
         const css = fpath.node.children[0].value;
+        const cf = codeFrame(fpath.hub.file.code, fpath.node.loc.start.line, fpath.node.loc.start.column, { highlightCode: true });
 
-        PreStyle(css, config)
-          .then((data) => {
-            console.log('SUCCESS');
-            console.log(data);
-          });
+        PreStyle(css, config).then((data) => {
+          console.log('SUCCESS');
+          console.log(data);
+        }, (e) => {
+          console.log(chalk.red(`The PreStyle Adapter ran into an error: ${chalk.bold(e)}`));
+          console.log(cf);
+          process.exit();
+        });
       }
     }
   };

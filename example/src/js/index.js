4687964086164726fc61b0eef06643229af1a8e1
Ben Solum <benjamin@soluml.com>
2017-04-05 17:19:56 -0500

    enable HMR

--- example/src/js/index.js
+++ example/src/js/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import Wrapper from './wrapper';
 import '../css/app.scss';
 
+if (module.hot) {
+  module.hot.accept();
+}
+
 ReactDOM.render(<Wrapper />, document.getElementById('wrapper'));

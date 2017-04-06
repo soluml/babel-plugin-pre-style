d844796455b001ccbb901fb59c22d75123955ec4
Ben Solum <benjamin@soluml.com>
2017-04-06 08:59:36 -0500

    More interactive example

--- example/src/js/wrapper.jsx
+++ example/src/js/wrapper.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 
+function joinCSSClasses(...classNames) {
+  return classNames
+    .filter(className => className)
+    .join(' ');
+}
+
 export default class wrapper extends React.Component {
+  constructor() {
+    super();
+    this.state = { tick: 0 };
+  }
+
   render() {
+    const tickMod = this.state.tick % 3;
     const attrs = {
-      div: {
-        className: PreStyle`
-          color: blue;
-          font-weight: bold;
-          text-align: center;
-        `
+      h1: {
+        className: joinCSSClasses(
+          PreStyle`
+            font-weight: bold;
+            text-align: center;
+            transition: color .115s ease-in;
+          `,
+          (tickMod === 0 && PreStyle`color: red;`),
+          (tickMod === 1 && PreStyle`color: blue;`),
+          (tickMod === 2 && PreStyle`color: green;`)
+        )
       }
     };
 
-    return (<div {...attrs.div}>
+    return (<h1 {...attrs.h1}>
       Hello World!
-    </div>);
+    </h1>);
+  }
+
+  componentDidMount() {
+    const tickFn = () => {
+      setTimeout(() => {
+        this.setState({ tick: this.state.tick + 1 });
+        tickFn();
+      }, 1000);
+    };
+
+    tickFn();
   }
 }

05b83ed86d3c9d62584a309f8bff60481edfd794
soluml <benjamin@soluml.com>
2019-08-01 15:16:30 -0500

    Updated Button Example

--- example/button/webpack.config.js
+++ example/button/webpack.config.js
@@ -1,13 +1,38 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Sass = require('sass');
+const path = require('path');
+
 module.exports = {
+  entry: {
+    bundle: './button.js'
+  },
+  mode: 'production',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-    }, {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader?-minimize&-import&-modules!postcss-loader!sass-loader',
-      exclude: /(node_modules|bower_components)/,
-    }]
-  }
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: Sass
+            },
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ]
 };
